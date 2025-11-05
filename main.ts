e25da6a923c2c55656053582a28bbff9e4a1b991
Hajia Rashid <hajiarash75@gmail.com>
2025-11-05 13:16:24 +0000

    Hajia

--- main.ts
+++ main.ts
@@ -3,3 +3,10 @@ console.log(main);
 
 var myName = "Daniel Klugah";
 console.log("My name is " + myName);
+
+var Hello
+console.log;
+var myname ="Hajia Rashid"
+var age = 56g
+var complextion = 'dark'
+
