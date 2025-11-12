25c279688de575d037948320ff294e79a35e1bd7
richard-nii <richardjnr10@gmail.com>
2025-11-12 11:09:20 +0000

    Part 1,2 and 3 completed

--- richard.ts
+++ richard.ts
@@ -23,6 +23,30 @@ let newProduct: Product = {
     createdAt: new Date(),
 };
 
+type UserID = string | number;
+let userId: UserID = "user327";
+let userId2: UserID = 327;
 
 
 
+function displayStatus(status: "Active" | "Inactive" | "Pending"): void {
+    console.log(`The status is: ${status}`);
+}
+
+displayStatus("Active");
+displayStatus("Inactive");
+displayStatus("Pending");
+
+
+enum ProductStatus {
+    Active = "Active",
+    Inactive = "Inactive",
+    Pending = "Pending",
+}
+
+function displayProductStatus(status: ProductStatus): void {
+    console.log(`The product status is: ${status}`);
+}
+displayProductStatus(ProductStatus.Active);
+displayProductStatus(ProductStatus.Inactive);
+displayProductStatus(ProductStatus.Pending);
